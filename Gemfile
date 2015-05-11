dfd606afbf12b08810c901d7632c886aa9e525d5
askl56 <amscott@openmailbox.org>
2015-05-12 00:29:36 +0100

    Adds devise user model

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'active_model_serializers'
+gem 'devise'
 group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-Rails.application.routes.draw do
-  # API Definiton
-  namespace :api, defaults: { format: :json },
-  constraints: { subdomain: 'api'}, path: '/' do
+require 'api_constraints'
 
-    scope module: :v1,
-    constraints: ApiConstraints.new(version: 1, default: true) do
-
+BazaarApi::Application.routes.draw do
+  devise_for :users
+  # Api definition
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
     end
   end
 end
