82aa94a4feb08d66e2c24c3ab6d23cf924daf820
askl56 <amscott@openmailbox.org>
2015-05-12 00:16:02 +0100

    modified API constraints

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # API Definiton
   namespace :api, defaults: { format: :json },
   constraints: { subdomain: 'api'}, path: '/' do
-    scope module: :v1 do
+
+    scope module: :v1,
+    constraints: ApiConstraints.new(version: 1, default: true) do
 
     end
   end
