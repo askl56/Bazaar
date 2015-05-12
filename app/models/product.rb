a41a43de0375de987f443c33f93874a491b279e8
askl56 <amscott@openmailbox.org>
2015-05-12 07:19:45 +0100

    Added product model barebones along with some validations

---
+++ app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ActiveRecord::Base
+  validates :title, :user_id, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 },
+                    presence: true
+
+  belongs_to :user
+
+end
