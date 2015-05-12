fc062c3b27de4c5f0f10e3f9b56b5a4b51d9f5a3
askl56 <amscott@openmailbox.org>
2015-05-12 02:45:33 +0100

    added user create

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,40 @@ describe Api::V1::UsersController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe "POST #CREATE" do
+    context "when user is successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when user is not created" do
+      before(:each) do
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678"}
+        post :create, { user: @invalid_user_attributes }, format: :json
+      end
+
+      it "renders an error json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json errors with reasons" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
