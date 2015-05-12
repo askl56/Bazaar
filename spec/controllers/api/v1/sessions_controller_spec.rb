f446558edadff2995be5656cdfc536e38b66f401
askl56 <amscott@openmailbox.org>
2015-05-12 04:35:06 +0100

    Added sessions controller create action

---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe Api::V1::SessionsController, type: :controller do
+  describe "POST #create" do
+
+    before(:each) do
+      @user = FactoryGirl.create :user
+    end
+
+    context "when the credentials are correct" do
+      before(:each) do
+        credentials = { email: @user.email, password: "12345678" }
+        post :create, { session: credentials }
+      end
+
+      it "returns the correct user record" do
+        @user.reload
+        expect(json_response[:auth_token]).to eql @user.auth_token
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when the credentials are incorrect" do
+      before(:each) do
+        credentials = { email: @user.email, password: "invalidpassword" }
+        post :create, { session: credentials }
+      end
+
+      it "returns a json with an error" do
+        expect(json_response[:errors]).to eql "Invalid email or password"
+      end
+
+      it { should respond_with 422 }
+
+    end
+
+  end
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
 
   config.before(:each, type: :controller) do
     include_default_accept_headers
