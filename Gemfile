2884ce78a5be7041e41b89974b4eb238254614b8
askl56 <amscott@openmailbox.org>
2015-05-12 01:07:24 +0100

    Adds shoulda matchers for spec refactors

--- Gemfile
+++ Gemfile
@@ -13,10 +13,11 @@ group :development, :test do
   gem 'byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
-  gem 'ffaker'
 end
 group :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  gem 'ffaker'
+  gem 'shoulda-matchers'
 end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,10 @@ describe User, type: :model do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation)}
 
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+
   it { should be_valid }
 end
