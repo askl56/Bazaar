8afb90b60ea4baee800f62f5a4211a0bcc70d79a
askl56 <amscott@openmailbox.org>
2015-05-12 03:00:28 +0100

    added update action to users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -52,4 +52,41 @@ describe Api::V1::UsersController, type: :controller do
       it { should respond_with 422 }
     end
   end
+
+  describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+          user: { email: "newmail@example.com" } }, format: :json
+      end
+
+      it "renders the json for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql "newmail@example.com"
+      end
+      it { should respond_with 200 }
+    end
+
+    context "when is not updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, { id: @user.id,
+          user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders json errors" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the errors on why the user was not created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { should respond_with 422}
+    end
+  end
 end
