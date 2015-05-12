53188b177469a23357ec3cb75ec49fda43223445
askl56 <amscott@openmailbox.org>
2015-05-12 04:40:30 +0100

    added destroy functionality to sessions

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -32,9 +32,16 @@ describe Api::V1::SessionsController, type: :controller do
       end
 
       it { should respond_with 422 }
-
     end
-
   end
 
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      sign_in @user
+      delete :destroy, id: @user.auth_token
+    end
+
+    it { should respond_with 204 }
+  end
 end
