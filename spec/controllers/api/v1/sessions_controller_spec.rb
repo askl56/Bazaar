9bb48aee56bd11b4d9552383540e378405c26977
askl56 <amscott@openmailbox.org>
2015-05-13 16:47:52 +0100

    Embedding the user into the product json output

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -15,7 +15,7 @@ describe Api::V1::SessionsController, type: :controller do
 
       it "returns the correct user record" do
         @user.reload
-        expect(json_response[:auth_token]).to eql @user.auth_token
+        expect(json_response[:user][:auth_token]).to eql @user.auth_token
       end
 
       it { should respond_with 200 }
