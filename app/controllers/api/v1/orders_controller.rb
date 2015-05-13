a8718ccfef1929e2854969db7ecaec331c26eefd
askl56 <amscott@openmailbox.org>
2015-05-13 18:24:27 +0100

    Added quantity to placements

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -11,10 +11,12 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def create
-    order = current_user.orders.build(order_params)
+    order = current_user.orders.build
+    order.build_placements_with_product_ids_and_quantities(params[:order][:product_ids_and_quantities])
 
     if order.save
-      OrderMailer.send_confirmation(order).deliver_now
+      order.reload
+      OrderMailer.send_confirmation(order).deliver
       render json: order, status: 201, location: [:api, current_user, order]
     else
       render json: { errors: order.errors }, status: 422
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513162003) do
+ActiveRecord::Schema.define(version: 20150513172209) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(version: 20150513162003) do
   create_table "placements", force: :cascade do |t|
     t.integer  "order_id"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "quantity",   default: 0
   end
 
   add_index "placements", ["order_id"], name: "index_placements_on_order_id"
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150513162003) do
     t.integer  "user_id"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "quantity",   default: 0
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id"
