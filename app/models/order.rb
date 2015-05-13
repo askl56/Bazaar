648ba199622408aa4810679f0e873780c953ea2e
askl56 <amscott@openmailbox.org>
2015-05-13 17:17:17 +0100

    Added Order model

---
+++ app/models/order.rb
@@ -0,0 +1,8 @@
+class Order < ActiveRecord::Base
+  belongs_to :user
+
+  validates :total, presence: true,
+    numericality: { greater_than_or_equal_to: 0 }
+
+  validates :user_id, presence: true
+end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Order, type: :model do
+  let(:order) { FactoryGirl.build :order }
+  subject { order }
+
+  it { should respond_to(:total) }
+  it { should respond_to(:user_id) }
+
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :total }
+  it { should validate_numericality_of(:total).is_greater_than_or_equal_to(0) }
+
+  it { should belong_to :user }
+end
