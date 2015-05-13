4c30a48230c43746920d24f7a59b999e3fcd168b
askl56 <amscott@openmailbox.org>
2015-05-13 18:32:37 +0100

    added decremental placements

--- app/models/placement.rb
+++ app/models/placement.rb
@@ -1,4 +1,8 @@
 class Placement < ActiveRecord::Base
   belongs_to :order, inverse_of: :placements
   belongs_to :product, inverse_of: :placements
+
+  def decrement_product_quantity!
+    self.product.decrement!(:quantity, quantity)
+  end
 end
