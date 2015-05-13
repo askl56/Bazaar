0911e0a0b363d21bb8753e14383a052a5012ddb0
askl56 <amscott@openmailbox.org>
2015-05-13 17:08:19 +0100

    added search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,23 @@
 class Product < ActiveRecord::Base
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 },
-                    presence: true
+    presence: true
 
   belongs_to :user
 
+  scope :filter_by_title, lambda { |keyword|
+    where("lower(title) LIKE ?", "%#{keyword.downcase}%" )
+  }
+
+  scope :above_or_equal_to_price, lambda { |price|
+    where("price >= ?", price)
+  }
+
+  scope :below_or_equal_to_price, lambda { |price|
+    where("price <= ?", price)
+  }
+
+  scope :recent, -> {
+    order(:updated_at)
+  }
 end
