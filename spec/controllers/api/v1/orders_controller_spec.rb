9a0943ab31fa14f421a8ff6768db2a064c711ba8
askl56 <amscott@openmailbox.org>
2015-05-13 18:07:03 +0100

    added custom order product serializer to remove the user association

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -21,17 +21,50 @@ describe Api::V1::OrdersController, type: :controller do
     before(:each) do
       current_user = FactoryGirl.create :user
       api_authorization_header current_user.auth_token
-      @order = FactoryGirl.create :order, user: current_user
+
+      @product = FactoryGirl.create :product
+      @order = FactoryGirl.create :order, user: current_user, product_ids: [@product.id]
       get :show, user_id: current_user.id, id: @order.id
     end
 
-    it "returns the user order record matching the ID" do
+    it "returns the user order record matching the id" do
       order_response = json_response[:order]
       expect(order_response[:id]).to eql @order.id
     end
 
+    it "includes the total for the order" do
+      order_response = json_response[:order]
+      expect(order_response[:total]).to eql @order.total.to_s
+    end
+
+    it "includes the products on the order" do
+      order_response = json_response[:order]
+      expect(order_response[:products]).to have(1).item
+    end
+
     it { should respond_with 200 }
   end
+
+  describe "POST #create" do
+    before(:each) do
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
+
+      product_1 = FactoryGirl.create :product
+      product_2 = FactoryGirl.create :product
+      order_params = { product_ids: [product_1.id, product_2.id] }
+      post :create, user_id: current_user.id, order: order_params
+    end
+
+    it "returns the just user order record" do
+      order_response = json_response[:order]
+      expect(order_response[:id]).to be_present
+    end
+
+    it { should respond_with 201 }
+  end
+
+
 
   describe "POST #create" do
     before(:each) do
